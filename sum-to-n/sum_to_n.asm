f2db46b5de4763888f095ebda2a934e36e1ab4d6
Guanran Tai <taiguanran@gmail.com>
2024-05-04 17:06:35 +0200

    implement sum_to_n

--- sum-to-n/sum_to_n.asm
+++ sum-to-n/sum_to_n.asm
@@ -1,4 +1,22 @@
 section .text
 global sum_to_n
 sum_to_n:
+	; without LOOP 
+	; MOV rax, rdi
+	; ADD rax, 1
+	; IMUL rax, rdi
+	; SHR  rax, 1
+	; ret
+	xor rax, rax
+	xor rdx, rdx 
+	ADD rax, rdx
+	CMP rdx, rdi
+	JNE .LP
+	JMP .exit
+.LP:	
+	INC rdx
+	ADD rax, rdx
+	CMP rdx, rdi
+	JNE .LP
+.exit:
 	ret
