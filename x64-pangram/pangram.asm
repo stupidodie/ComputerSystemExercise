761149578f34d5215f238672ca786b65287f5cc5
Guanran Tai <taiguanran@gmail.com>
2024-05-04 19:38:17 +0200

    Use bts to optimize

--- x64-pangram/pangram.asm
+++ x64-pangram/pangram.asm
@@ -2,4 +2,21 @@ section .text
 global pangram
 pangram:
 	; rdi: source string
+	
+	xor edx, edx
+	xor ecx, ecx
+._LOOP:
+	mov cl,[rdi]
+	INC rdi
+	cmp cl, 0
+	je ._compare
+	cmp cl, '@'
+	jl ._LOOP
+	bts edx, ecx
+	jmp ._LOOP
+._compare:
+	and edx,0x07fffffe
+	cmp edx, 0x07fffffe 
+	sete al
+	movzx eax, al
 	ret
