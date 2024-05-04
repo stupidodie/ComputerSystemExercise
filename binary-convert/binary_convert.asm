66d80621ed29640dff8f17f5938b92361eaad326
Guanran Tai <taiguanran@gmail.com>
2024-05-04 20:13:56 +0200

    Use movzx to optimize

--- binary-convert/binary_convert.asm
+++ binary-convert/binary_convert.asm
@@ -2,14 +2,13 @@ section .text
 global binary_convert
 binary_convert:
 	xor eax, eax
-	xor ecx, ecx
 ._loop:
-	mov cl, [rdi]
-	cmp cl, 0
+	movzx ecx, byte [rdi]
+	cmp ecx, 0
 	je ._exit
 	shl eax, 1
 	inc rdi
-	sub cl, '0'
+	sub ecx, '0'
 	add eax, ecx
 	jmp ._loop
 ._exit:
