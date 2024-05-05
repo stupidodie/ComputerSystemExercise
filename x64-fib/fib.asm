e80d13759658073fe5769f4e9c222ee7d4f5fc89
Guanran Tai <taiguanran@gmail.com>
2024-05-05 23:22:44 +0200

    Add fib recursive

--- x64-fib/fib.asm
+++ x64-fib/fib.asm
@@ -1,4 +1,20 @@
 section .text
 global fib
 fib:
+	cmp edi,2
+	jl .basic_case
+	jmp .cal
+.cal:
+	dec edi
+	push rdi
+	call fib ; fib(n-1)
+	pop rdi
+	push rax
+	dec edi
+	call fib ; fib(n-2)
+	pop rdx
+	add eax, edx
+	ret 
+.basic_case:
+	mov eax, edi 
 	ret
